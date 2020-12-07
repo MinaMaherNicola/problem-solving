3024395d5fd9d4f1e1b50bab55f5cf3c8a59b117
MinaMaherNicola <minamahernicola@outlook.com>
2020-12-07 22:28:49 +0200

    Solved oranges & apples in nodeJS

---
+++ problem-solving/apples-oranges.py
@@ -0,0 +1,7 @@
+# INPUTS
+# s: int, starting point of Sam's house location
+# t: int, ending point of Sam's house location
+# a: int, location of apple tree
+# b: int, location of orange tree
+# apples: int array, distance at which each apple falls from the tree
+# oranges: int array, distance at which each apple falls from the tree
