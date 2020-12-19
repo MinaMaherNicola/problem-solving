879774e0bca2179df75ccfbd9899bdaeaf611b1e
MinaMaherNicola <minamahernicola@outlook.com>
2020-12-19 15:26:13 +0200

    Finished testing with github

--- problem-solving/birthday-cake-problem.js
+++ problem-solving/birthday-cake-problem.js
@@ -1,13 +1,13 @@
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 
 const arrSize = prompt();
 let candlesHeight = prompt();
-candlesHeight = candlesHeight.split(' ');
+candlesHeight = candlesHeight.split(" ");
 for (let i = 0; i < arrSize; i++) {
   candlesHeight[i] = parseInt(candlesHeight[i]);
 }
 
-const birthdayCakeCandles = candles => {
+const birthdayCakeCandles = (candles) => {
   let maxHolder = candles[0];
   let maxCounter = 0;
   for (let i = 0; i < candles.length; i++) {
@@ -20,7 +20,6 @@ const birthdayCakeCandles = candles => {
     }
   }
   return maxCounter;
-}
+};
 
 console.log(birthdayCakeCandles(candlesHeight));
-// new nlienrfweingft'wel;gmfs;
